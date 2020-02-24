c5f605af43a0a3c4e2092915a0e8864bbcf1c6d1
iryabuhin <ryabuchin@gmail.com>
2020-02-24 22:29:32 +0300

    Modified config file

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ uploads_dir = os.path.join(basedir, 'uploads')
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'SwC@z4m23i2dkmd^bZN@Yw'
-    FLASK_ENV = os.environ.get('FLASK_ENV') or 'development'
-    FLASK_DEBUG = os.environ.get('FLASK_DEBUG') or 1
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
+    FLASK_ENV = os.environ.get('FLASK_ENV', 'development')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG', 1)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_IMAGES_DEST = os.path.join(basedir, 'uploads')
